af86b6c95cf7f655fc3bd26b4700a026c263e5c0
hanati <hanati@tezign.com>
2019-07-19 13:02:57 +0800

    gen data

--- dataset/mk_data.py
+++ dataset/mk_data.py
@@ -10,11 +10,11 @@ import copy
 
 from multiprocessing import Pool
 
-back_ground_dir = 'bg/'
+back_ground_dir = 'bg_old/'
 
-font_data_dir = 'fonts/'
+font_data_dir = '印刷体/'
 
-single_font_data_dir = 'single/'
+single_font_data_dir = 'train/'
 
 multi_font_data_dir = 'mix/'
 
